e8c1a79ee7ccb6170abcd2051e0bbf1bfe1db1da
Ronnie Song <ronniesong0809@gmail.com>
2021-06-18 15:10:21 -0700

    add expandable row and searchable filter

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,12 +1,26 @@
 <template>
   <v-card>
     <v-card-title>
+      Questions
+      <v-spacer> </v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
     </v-card-title>
     <v-data-table
       :headers="headers"
       :items="questions"
       :items-per-page="20"
+      :search="search"
       multi-sort
+      :single-expand="true"
+      :expanded.sync="expanded"
+      item-key="id"
+      show-expand
       class="elevation-1"
     >
       <template v-slot:[`item.title`]="{ item }">
@@ -15,7 +29,14 @@
         </a>
       </template>
       <template v-slot:[`item.level`]="{ item }">
+        <!-- <v-chip :color="getColor(item.level)" dark> -->
         {{ item.level === 1 ? "Easy" : item.level == 2 ? "Medium" : "Hard" }}
+        <!-- </v-chip> -->
+      </template>
+      <template v-slot:expanded-item="{ headers, item }">
+        <td :colspan="headers.length">
+          Search <a :href="'https://www.youtube.com/results?search_query=' + item.title">{{ item.title }}</a> on YouTube
+        </td>
       </template>
     </v-data-table>
   </v-card>
@@ -29,7 +50,8 @@ const headers = [
   { text: "Title", value: "title" },
   { text: "Level", value: "level" },
   { text: "Frequency", value: "frequency" },
-  { text: "Tags", value: "tags", sortable: false }
+  { text: "Tags", value: "tags", sortable: false },
+  { text: "", value: "data-table-expand" }
 ];
 /* eslint-disable */
 export default {
@@ -37,6 +59,7 @@ export default {
   data() {
     return {
       search: "",
+      expanded: [],
       headers,
       questions: []
     };
@@ -45,6 +68,11 @@ export default {
     this.fetchQuestions();
   },
   methods: {
+    getColor(level) {
+      if (level == 1) return "green";
+      else if (level == 2) return "orange";
+      else return "red";
+    },
     async fetchQuestions() {
       getAllQuestions().then(value => {
         const data = value.data;
