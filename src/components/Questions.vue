34ea204f0bb36b5131bd0010814c21dff08c2d42
Ronnie Song <ronniesong0809@gmail.com>
2021-06-18 17:57:49 -0700

    refactor the frequency by using the progress bar

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -14,7 +14,7 @@
     <v-data-table
       :headers="headers"
       :items="questions"
-      :items-per-page="20"
+      :items-per-page="50"
       :search="search"
       multi-sort
       :single-expand="true"
@@ -24,18 +24,42 @@
       class="elevation-1"
     >
       <template v-slot:[`item.title`]="{ item }">
-        <a :href="item.url">
+        <a :href="item.url" class="green--text text--darken-4">
           {{ item.title }}
         </a>
       </template>
       <template v-slot:[`item.level`]="{ item }">
-        <!-- <v-chip :color="getColor(item.level)" dark> -->
-        {{ item.level === 1 ? "Easy" : item.level == 2 ? "Medium" : "Hard" }}
-        <!-- </v-chip> -->
+        <v-chip small :color="getChipColor(item.level)" dark>
+          {{
+            item.level === 1
+              ? "Easy 🍀"
+              : item.level === 2
+              ? "Medium"
+              : "Hard 🔥"
+          }}
+        </v-chip>
+      </template>
+      <template v-slot:[`item.frequency`]="{ item }">
+        <template>
+          <v-progress-linear
+            :color="getBarColor(item.frequency * 25)"
+            :value="item.frequency * 25"
+            :buffer-value="item.frequency * 25"
+            rounded
+            stream
+          >
+          </v-progress-linear>
+        </template>
       </template>
       <template v-slot:expanded-item="{ headers, item }">
         <td :colspan="headers.length">
-          Search <a :href="'https://www.youtube.com/results?search_query=' + item.title">{{ item.title }}</a> on YouTube
+          Search
+          <a
+            :href="'https://www.youtube.com/results?search_query=' + item.title"
+          >
+            {{ item.title }}
+          </a>
+          on YouTube
         </td>
       </template>
     </v-data-table>
@@ -46,14 +70,14 @@
 import { getAllQuestions } from "@/apis/getQuestions";
 
 const headers = [
-  { text: "Id", align: "start", value: "id" },
+  { text: "#", align: "start", value: "id" },
   { text: "Title", value: "title" },
-  { text: "Level", value: "level" },
-  { text: "Frequency", value: "frequency" },
+  { text: "Difficulty", value: "level" },
+  { text: "Frequency", value: "frequency", filterable: false },
   { text: "Tags", value: "tags", sortable: false },
   { text: "", value: "data-table-expand" }
 ];
-/* eslint-disable */
+
 export default {
   name: "Questions",
   data() {
@@ -68,10 +92,18 @@ export default {
     this.fetchQuestions();
   },
   methods: {
-    getColor(level) {
+    getChipColor(level) {
       if (level == 1) return "green";
       else if (level == 2) return "orange";
       else return "red";
+    },
+    getBarColor(frequency) {
+      if (frequency === 0) return "grey lighten-1";
+      if (frequency > 0 && frequency < 25) return "red lighten-1";
+      if (frequency >= 25 && frequency < 50) return "orange lighten-1";
+      if (frequency >= 50 && frequency < 75) return "green lighten-1";
+      else if (frequency >= 75 && frequency < 99) return "green darken-2";
+      else return "blue darken-1";
     },
     async fetchQuestions() {
       getAllQuestions().then(value => {
@@ -82,4 +114,8 @@ export default {
   }
 };
 </script>
-/* eslint-enable */
+<style>
+a {
+  text-decoration: none;
+}
+</style>
