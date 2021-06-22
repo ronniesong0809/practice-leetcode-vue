d654e6fb6a796c63d5fedf723a3422afc3f1404a
Ronnie Song <ronniesong0809@gmail.com>
2021-06-21 20:58:40 -0700

    add gtag

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VueGtag from "vue-gtag";
 
 Vue.use(Vuex);
+
+Vue.use(VueGtag, {
+  config: { id: "G-M0ZWB8YB40" }
+});
 
 export default new Vuex.Store({
   state: {},
