aacf185abe86e6f072434e37fa1c284056f0bb5e
Ronnie Song <ronniesong0809@gmail.com>
2021-06-22 01:55:20 -0700

    twist the fab color theme

--- src/components/DarkTheme.vue
+++ src/components/DarkTheme.vue
@@ -1,13 +1,13 @@
 <template>
   <v-fab-transition v-if="!$vuetify.theme.dark">
     <v-btn class="moon" fab dark color="black" @click="darkMode">
-      <v-icon color="yellow lighten-1">mdi-weather-night</v-icon>
+      <v-icon color="yellow">mdi-weather-night</v-icon>
     </v-btn>
   </v-fab-transition>
 
   <v-fab-transition v-else>
     <v-btn class="sun" fab dark color="primary" @click="darkMode">
-      <v-icon color="yellow lighten-1">mdi-white-balance-sunny</v-icon>
+      <v-icon color="yellow">mdi-white-balance-sunny</v-icon>
     </v-btn>
   </v-fab-transition>
 </template>
@@ -23,12 +23,20 @@ export default {
 </script>
 
 <style>
-/* .sun {
+.sun {
   animation: rotation 4s infinite linear;
-} */
+}
+
+.sun:hover {
+  animation: none;
+}
 
 .moon {
+  animation: swing 1s infinite linear;
+}
+
+.moon:hover {
+  animation: none;
   transform: rotate(-90deg);
-  /* animation: swing 2s infinite linear; */
 }
 </style>
